46b133794ff1f5b9d1975825f2ddde220652ce4c
Aryan <akaryankr17@gmail.com>
2025-01-27 17:32:58 +0530

    update font size

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -38,21 +38,24 @@ const Reviews = () => {
   return (
     <div className='w-full min-h-screen flex gap-8 items-center justify-center'>
         <div ref={reviewRef1} className='w-[30%] h-auto  flex flex-col gap-8 justify-center items-center'>
-            <div className='w-[90%] bg-white h-[15rem] rounded-3xl p-5'>
+
+            <div className='w-[90%] bg-white min-h-[15rem] rounded-3xl p-5'>
                 <h2 className='text-[#3482ff] font-bold text-3xl font-[font1] mb-3'>Himanshu <span className='text-xl '>⭐⭐⭐⭐⭐</span> </h2>
                 <p className='text-gray-800 font-semibold text-md'>I used Indolike Web design Service for developing my portfolio website, and I couldn’t be happier. The design is sleek, professional, and responsive. Their team understood my vision, delivered on time, and provided excellent support throughout the process.</p>
             </div>
-            <div className='w-[90%] bg-white h-[15rem] rounded-3xl p-5'>
+            <div className='w-[90%] bg-white min-h-[15rem] rounded-3xl p-5'>
                 <h2 className='text-[#3482ff] font-bold text-3xl font-[font1] mb-3'>Divya <span className='text-xl '>⭐⭐⭐⭐</span> </h2>
                 <p className='text-gray-800 font-semibold text-md'>I’ve been using Indolike for SMM services, and the experience has been fantastic. Their platform is easy to use, offers quick delivery, and provides genuine results. Excellent customer support, making it a reliable choice!</p>
             </div>
         </div>
+
         <div ref={reviewRef2} className='w-[30%] h-auto  flex flex-col gap-8 justify-center items-center'>
-            <div className='w-[90%] bg-white h-[15rem] rounded-3xl p-5'>
+
+            <div className='w-[90%] bg-white min-h-[15rem] rounded-3xl p-5'>
                 <h2 className='text-[#3482ff] font-bold text-3xl font-[font1] mb-3'>Mansu <span className='text-xl '>⭐⭐⭐⭐⭐</span> </h2>
                 <p className='text-gray-800 font-semibold text-md'>I tryed Indolike’s SEO services, and the results have been outstanding. My website’s ranking has improved significantly, driving more organic traffic. Their team is professional, responsive, and delivers effective strategies. Highly recommend!</p>
             </div>
-            <div className='w-[90%] bg-white h-[15rem] rounded-3xl p-5'>
+            <div className='w-[90%] bg-white min-h-[15rem] rounded-3xl p-5'>
                 <h2 className='text-[#3482ff] font-bold text-3xl font-[font1] mb-3'>Abhishek <span className='text-xl '>⭐⭐⭐</span> </h2>
                 <p className='text-gray-800 font-semibold text-md'>My internship at Indolike was an incredible learning experience. I gained hands-on skills in web development, worked on real projects, and received valuable mentorship. The team is supportive, making it an enriching environment for growth.</p>
             </div>
